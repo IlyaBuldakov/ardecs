2a7670fbc081b7602121059a9bf85806fd68877b
IlyaBuldakov <ggpanguin@gmail.com>
2023-11-08 12:51:47 +0400

    Добавлена стратегия реализации кэша по обратному числовому приоритету (MFU)

---
+++ src/main/java/org/buldakov/model/cache/strategy/MfuCacheStrategy.java
@@ -0,0 +1,15 @@
+package org.buldakov.model.cache.strategy;
+
+import org.buldakov.model.cache.CacheMetaDataEntry;
+
+import java.util.Comparator;
+
+public class MfuCacheStrategy extends IntegerPriorityCacheStrategy {
+
+    private static final Comparator<CacheMetaDataEntry<Integer>> MFU_COMPARATOR
+            = (entry1, entry2) -> -(entry1.getPriority().compareTo(entry2.getPriority()));
+
+    public MfuCacheStrategy() {
+        super(MFU_COMPARATOR);
+    }
+}
