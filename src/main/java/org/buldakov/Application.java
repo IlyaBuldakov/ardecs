201b61e963ce85a2e793e2c8c76a61f8f210bb71
IlyaBuldakov <ggpanguin@gmail.com>
2023-11-09 12:49:45 +0400

    Добавлен парсер входных параметров, изменены тест.данные

--- src/main/java/org/buldakov/Application.java
+++ src/main/java/org/buldakov/Application.java
@@ -4,22 +4,45 @@ import org.buldakov.model.cache.CacheStorage;
 import org.buldakov.model.cache.CacheStorageBuilder;
 import org.buldakov.model.cache.enums.DeleteStrategyType;
 
+import java.io.IOException;
+import java.util.Locale;
+
 public class Application {
 
-    public static void main(String[] args) {
-        lruTest();
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        DeleteStrategyType strategy = parseStrategy(args);
+        cacheTest(strategy);
     }
 
-    private static void lfuTest() {
-        CacheStorageBuilder<Integer> builder = new CacheStorageBuilder<>();
-        CacheStorage<Integer> storage = builder
+    private static DeleteStrategyType parseStrategy(String[] args) {
+        DeleteStrategyType result = DeleteStrategyType.LFU;
+        if (args.length == 1) {
+            try {
+                result = DeleteStrategyType.valueOf(args[0].toUpperCase(Locale.ROOT));
+            } catch (IllegalArgumentException exception) {
+                System.err.println("Неверный параметр");
+            }
+        } else {
+            System.err.println("Ожидается на вход 1 параметр - стратегия. По умолчанию использована LFU");
+        }
+        return result;
+    }
+
+    private static void cacheTest(DeleteStrategyType deleteStrategy) throws IOException, ClassNotFoundException {
+        CacheStorageBuilder builder = new CacheStorageBuilder();
+        CacheStorage storage = builder
                 .setCapacity(2)
-                .setCacheStrategy(DeleteStrategyType.LFU)
+                .setCacheStrategy(deleteStrategy)
+                .enableL2Cache()
                 .build();
+
+        System.out.printf("=== Стратегия %s ===%n", deleteStrategy);
 
         // Добавляем тестовые элементы
         storage.addData("Cache1", 1);
         storage.addData("Cache2", 1);
+
+        storage.getData("Cache2");
 
         // Два раза используем Cache1
         storage.getData("Cache1");
@@ -33,70 +56,22 @@ public class Application {
 
         // Добавляем тестовый элемент
         storage.addData("Cache4", 1);
+
+        System.out.printf("=== Стратегия %s ===", deleteStrategy);
 
         /* Согласно стратегии LFU, будут удалены элементы с наименьшим использованием на момент добавления
          * нового элемента в кэш. Cache1 будет существовать до конца, как элемент с наибольшим приоритетом.
          * Cache3 будет добавлен и один раз использован, но также будет беспощадно вытеснен из кэша из-за принудительного
          * добавления элемента Cache4, в процессе которого будут сравниваться Cache1 (2 исп. ) и Cache3 (1 исп.) */
-    }
 
-    private static void mfuTest() {
-        CacheStorageBuilder<Integer> builder = new CacheStorageBuilder<>();
-        CacheStorage<Integer> storage = builder
-                .setCapacity(2)
-                .setCacheStrategy(DeleteStrategyType.MFU)
-                .build();
-
-        // Добавляем тестовые элементы
-        storage.addData("Cache1", 1);
-        storage.addData("Cache2", 1);
-
-        // Два раза используем Cache1
-        storage.getData("Cache1");
-        storage.getData("Cache1");
-
-        // Добавляем тестовый элемент
-        storage.addData("Cache3", 1);
-
-        // Один раз используем Cache3
-        storage.getData("Cache3");
-
-        // Добавляем тестовый элемент
-        storage.addData("Cache4", 1);
-
-        /* Те же тестовые данные, но MFU конфигурация хранилища. На выходе получаем хранилище с элементами
-        * Cache2 и Cache4, как наименее использованные (не использованные) элементы хранилища.
-        * Эта стратегия - как LFU, только с "-" в компараторе, и, как следствие, обратным приоритетом. */
-    }
-
-    private static void lruTest() {
-        CacheStorageBuilder<Integer> builder2 = new CacheStorageBuilder<>();
-        CacheStorage<Integer> storage2 = builder2
-                .setCapacity(2)
-                .setCacheStrategy(DeleteStrategyType.LRU)
-                .build();
-
-        // Добавляем тестовые элементы
-        storage2.addData("Cache1", 1);
-        storage2.addData("Cache2", 1);
-
-        // Два раза используем Cache1
-        storage2.getData("Cache1");
-        storage2.getData("Cache1");
-
-        // Добавляем тестовый элемент
-        storage2.addData("Cache3", 1);
-
-        // Один раз используем Cache3
-        storage2.getData("Cache3");
-
-        // Добавляем тестовый элемент
-        storage2.addData("Cache4", 1);
+        /*  MFU конфигурация хранилища. На выходе получаем хранилище с элементами
+         * Cache2 и Cache4, как наименее использованные (не использованные) элементы хранилища.
+         * Эта стратегия - как LFU, только с "-" в компараторе, и, как следствие, обратным приоритетом. */
 
         /* В отличии от предыдущих стратегий, LRU стратегия будет вытеснять те элементы, которые использованы
-        * раннее (более давно). Таким образом привычный тест оборачивается неожиданным результатом, поскольку
-        * в процессе добавления Cache4 перед стратегией предстает дилемма - кого выгнать? Cache1 или Cache3?
-        * Без лишних сожалений стратегия вытесняет Cache1, несмотря на высокий клиентский спрос, так как
-        * использовали её давно :\ */
+         * раннее (более давно). Таким образом привычный тест оборачивается неожиданным результатом, поскольку
+         * в процессе добавления Cache4 перед стратегией предстает дилемма - кого выгнать? Cache1 или Cache3?
+         * Без лишних сожалений стратегия вытесняет Cache1, несмотря на высокий клиентский спрос, так как
+         * использовали её давно :\ */
     }
 }
